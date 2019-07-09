30720c7083d6338beb68f3961abb9ef575f28f98
JerYoMat <jeremy.emata@gmail.com>
2019-07-09 16:53:55 -0400

    moved namedColumns to checkAndAssignData.js - can just reference data from there - trying to keep index lean

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const hub = require('./lib/checkAndAssignData');
 
 (async () => {
   const data = await myCSVUtil.parseData('./data/Data.csv');
-  const namedColumns = {};
   data.forEach(row => {
     for (const column in row) {
-      if (namedColumns.hasOwnProperty(column)) {
-        namedColumns[column].push(row[column]);
-      } else {
-        namedColumns[column] = [row[column]];
-      }
+      hub.addNamedColumn(column, row)
     }
   });
-  hub.addDataThatAlignsWithTemplate(namedColumns)
+  hub.addDataThatAlignsWithTemplate()
 })();
+
+
 
